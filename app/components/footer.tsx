35d935a2fe5f7c897b10035c4a6788cd92d116bd
Darren <darViloria04@gmail.com>
2026-03-25 15:59:20 -0400

    Edited Footer Links
    Formatting not complete

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -18,44 +18,29 @@ function ArrowIcon() {
 export default function Footer() {
   return (
     <footer className="mb-16">
-      <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
+
+      <hr className="border-netural-300 dark:border-neutral-700" />
+
+      <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300 italic">
+
         <li>
-          <a
-            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
-            rel="noopener noreferrer"
-            target="_blank"
-            href="/rss"
-          >
-            <ArrowIcon />
-            <p className="ml-2 h-7">rss</p>
-          </a>
+          <a className="transition-all hover:text-neutral-800 dark:hover:text-neutral-100" href="mailto:darViloria04@gmail.com">email</a>
         </li>
+
         <li>
-          <a
-            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
-            rel="noopener noreferrer"
-            target="_blank"
-            href="https://github.com/vercel/next.js"
-          >
-            <ArrowIcon />
-            <p className="ml-2 h-7">github</p>
-          </a>
+          <a className="transition-all hover:text-neutral-800 dark:hover:text-neutral-100" href="https://github.com/wirydotrocks">github</a>
         </li>
+
         <li>
-          <a
-            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
-            rel="noopener noreferrer"
-            target="_blank"
-            href="https://vercel.com/templates/next.js/portfolio-starter-kit"
-          >
-            <ArrowIcon />
-            <p className="ml-2 h-7">view source</p>
-          </a>
+          <a className="transition-all hover:text-neutral-800 dark:hover:text-neutral-100" href="https://linkedin.com/in/darViloria04">linkedin</a>
         </li>
+
       </ul>
-      <p className="mt-8 text-neutral-600 dark:text-neutral-300">
-        © {new Date().getFullYear()} MIT Licensed
+
+      <p className="mt-8 font-xs text-neutral-600 dark:text-neutral-300 italic">
+        @ {new Date().getFullYear()} Darren Viloria 
       </p>
+
     </footer>
   )
 }
