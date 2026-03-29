f5b879ead0f9a83349089ad9adf1bdbcbe7b96b1
Darren <darViloria04@gmail.com>
2026-03-29 18:28:22 -0400

    Top Album 3x3 Grid added

--- app/components/TopAlbums.tsx
+++ app/components/TopAlbums.tsx
@@ -1,1 +1,53 @@
-deeez
+'use client';
+import React, { useState, useEffect } from 'react';
+import Marquee from 'react-fast-marquee';
+
+export const TopAlbums = ({ userName, apiKey }: {userName: string; apiKey: string }) => {
+  const[lfmData, updateLfmData] = useState<any>({});
+  useEffect(() => {
+    fetch(`https://ws.audioscrobbler.com/2.0/?method=user.getTopAlbums&user=${userName}&api_key=${apiKey}&limit=9&period=overall&format=json`)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error('error');
+      })
+      .then(data => updateLfmData(data))
+      .catch(() =>
+        updateLfmData({ error: 'Whoops! Something went wrong with Last.fm' })
+      );
+  }, []);
+  
+  const buildLastFmData = () => {
+    const  { error } = lfmData;
+    const albums = lfmData?.topalbums?.album;
+  
+    if (error) {
+      return <p>{error}</p>;
+    }
+  
+    if (!albums) {
+      return <p>Loading</p>;
+    }
+
+    return( 
+
+           <div className="grid grid-cols-3 gap-1">
+              {albums.map((album: any) => (
+                <a key={album.name} href={album.url} target="_blank">
+                      <img src={album.image[2]['#text']} alt={album.name} />
+                </a>
+              ))}
+           </div>
+
+
+
+
+    );
+  
+  };
+
+  return buildLastFmData();
+
+};
+
--- app/music/page.tsx
+++ app/music/page.tsx
@@ -2,6 +2,8 @@
 
 import { CurrentlyListening } from 'app/components/CurrentlyListening';
 import { TopArtists } from 'app/components/TopArtists';
+import { TopAlbums } from 'app/components/TopAlbums';
+
 
 
 
@@ -41,8 +43,20 @@ export default function Page() {
 
       </div>
 
-       <div className=" text-2xl w-full md:w-1/2">
-        Column 2
+       <div className="text-2xl w-full md:w-1/2 pb-4">
+          <h1 className="underline pb-4">
+            Top Albums
+          </h1>
+ 
+
+       <TopAlbums
+          userName={'wirydotrocks'}
+          apiKey={process.env.NEXT_PUBLIC_LASTFM_API_KEY}
+       />
+
+
+
+
         </div>
 
       </div>
