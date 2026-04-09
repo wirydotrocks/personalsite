43eedc043be7d9f00aa55a9dd27d66e5fdce3b58
Darren <darViloria04@gmail.com>
2026-04-08 22:24:23 -0400

    Added blank resume pg

---
+++ app/resume/page.tsx
@@ -0,0 +1,20 @@
+
+const photos = [
+  { title: "herta", image: "/images/drawings"}
+
+
+]
+
+
+export default function Page() {
+  return (
+    <section>
+
+    <h1 className="text-3xl font-bold"
+    > 
+    Resume
+    </h1>
+
+    </section>
+  )
+}
